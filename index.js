6788913e9b6422c092c1a971b4645fb69cc4f7b4
Haron M Maina <haronmmaina@Harons-MacBook-Air.local>
2025-03-09 18:16:04 +0300

    Adds changes to index .html

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob'; 
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase ; 
+}
+upperCaseCustomerName(customerName);
+
+console.log(upperCaseCustomerName(customerName))
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob'  
+}
+console.log (overwriteBestCustomer())
+
+const leastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'stacy'
+}
+console.log(changeLeastFavoriteCustomer(leastFavoriteCustomer))
+
